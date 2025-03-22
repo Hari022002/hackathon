5fb75f50b1d23518b0ed1ec471c35566ca062600
Harish <harishthiyagu02@gmail.com>
2025-03-22 21:00:46 +0530

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+// Navbar.js
+import React from 'react';
+import './Navbar.css';
+import logo1 from './assets/tomcat.png';
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className='header_1'><h1 className="navbar-heading">My Website</h1></div>
+                <div className="logo-container">
+                    <img src={logo1} alt="Logo 1" className="navbar-logo" />
+                    <img src={logo1} alt="Logo 2" className="navbar-logo" />
+                    <img src={logo1} alt="Logo 3" className="navbar-logo" />
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
